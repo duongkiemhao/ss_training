cdd83eb1f7023a4919c34876c4a504aea22fffd0
GreenHornOx <phamhuuduc2971@gmail.com>
2023-05-15 16:54:45 +0700

    Fixed Communication between Activity/Fragment Fragment/Fragment

---
+++ Hao_Activity_2_Fixed/app/src/main/java/com/example/hao_activity_2_fixed/FirstFragment.java
@@ -0,0 +1,58 @@
+package com.example.hao_activity_2_fixed;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+// A simple {@link Fragment} subclass.
+
+public class FirstFragment extends Fragment {
+
+    Button red, blue, green;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        red = (Button) view.findViewById(R.id.red);
+        blue= (Button) view.findViewById(R.id.blue);
+        green = (Button) view.findViewById(R.id.green);
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_redFragment);
+            }
+        });
+
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_blueFragment);
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_greenFragment);
+            }
+        });
+
+
+        return view;
+    }
+}
